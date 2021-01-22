e41f36914a2137dcbffa270403ab97565c1c2eba
Isabelle Parker <iparker@ucsd.edu>
2021-01-22 14:07:19 +1000

    lab finished

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -16,15 +16,31 @@ function changeText(event){
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hey There :)");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
-console.log('some_string')
-$('#myElement').click(changeText);
+	$("a.thumbnail").click(projectClick);
+	// example: $("#div-id").click(functionToCall);\console.log('some_string')
+}
 
+$('#myElement').click(changeText);
 function changeText(event){
 	$(this).text("Changed text");
 }
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00")
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length ==0){
+		$(containingProject).append("<div class = 'project-description'><p>Description of the project.</p></div>");
+	} else{
+		$(containingProject).hide();
+	}
 }
